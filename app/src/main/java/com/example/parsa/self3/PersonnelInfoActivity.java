ca4c390494f1ab045b9efb6d3d777048c8d7402d
ashkan ghodrat <ashkanpower@gmail.com>
2015-01-29 09:11:20 +0330

    user activity action bar

--- app/src/main/java/com/example/parsa/self3/PersonnelInfoActivity.java
+++ app/src/main/java/com/example/parsa/self3/PersonnelInfoActivity.java
@@ -2,35 +2,88 @@ package com.example.parsa.self3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.parsa.self3.Helper.FontHelper;
 import com.example.parsa.self3.R;
 
-public class PersonnelInfoActivity extends Activity {
+public class PersonnelInfoActivity extends ActionBarActivity {
+
+    private TextView title;
+    private TextView logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personnel_info);
+
+
+
+        prepareActionBar();
     }
 
+    private void prepareActionBar() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.personnel_info, menu);
-        return true;
-    }
+        View customActionBar = getLayoutInflater().inflate(R.layout.actionbar_basket_view, null);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setCustomView(customActionBar);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        actionBar.setLogo(null); // forgot why this one but it helped
+        actionBar.setIcon(null);
+
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayUseLogoEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setHomeButtonEnabled(false);
+
+        View homeIcon = findViewById(android.R.id.home);
+
+        if (homeIcon != null) {
+            homeIcon.setVisibility(View.GONE);
         }
-        return super.onOptionsItemSelected(item);
+        if (homeIcon.getParent() != null) {
+            ((View) homeIcon.getParent()).setVisibility(View.GONE);
+        }
+
+
+        title = (TextView) customActionBar.findViewById(R.id.ac_title);
+        FontHelper.SetFontBold(this, FontHelper.Fonts.MAIN_FONT, title);
+
+        title.setText("بازگشت");
+
+
+        LinearLayout back = (LinearLayout) customActionBar.findViewById(R.id.ac_back_layout);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        logout = (TextView) customActionBar.findViewById(R.id.logout);
+
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //TODO logot user
+            }
+        });
+
+
+
     }
+
+
 }
